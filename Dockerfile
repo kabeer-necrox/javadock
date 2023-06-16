b7d6d2774ba6d8ba2373b37f8d095c7822b2e34b
kabeer-necrox <khushkabeer@gmail.com>
2023-06-16 07:04:32 +0300

    this my doc code

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ubuntu:20.04
+# RUN apt-get update
+RUN apt install default-jdk -y
+COPY . .
+RUN javac Main.java
+CMD ["java", Main]
---
+++ main.java
@@ -0,0 +1,7 @@
+Public Class Main(){
+    Public static void main (string[] args){
+        system.out.println("hello world")
+
+    }
+
+}
